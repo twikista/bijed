79527396cb2afd6ecea4da1296e5d2dc2d3267ee
Aaron Anama <twikista@gmail.com>
2024-01-22 15:09:02 +0100

    added not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+import MainContainer from '@/components/MainContainer'
+function NotFound() {
+  return (
+    <MainContainer>
+      <div>Not Found</div>
+    </MainContainer>
+  )
+}
+
+export default NotFound
