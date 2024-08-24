7cb4eb1e60421f817303d1e1bcbac0ed7eebd13c
Aaron Anama <twikista@gmail.com>
2024-08-25 00:11:22 +0100

    fixed evn error

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,14 +3,8 @@ export const config = {
     process.env.NODE_ENV === 'development'
       ? process.env.NEXT_URL
       : process.env.NEXT_BASE_URL,
-  storageBucket:
-    process.env.NODE_ENV === 'development'
-      ? process.env.NEXT_PUBLIC_STORAGE_BUCKET
-      : process.env.STORAGE_BUCKET,
-  sitekey:
-    process.env.NODE_ENV === 'development'
-      ? process.env.NEXT_PUBLIC_DEV_SITE_KEY
-      : process.env.SITE_KEY,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  sitekey: process.env.NEXT_PUBLIC_DEV_SITE_KEY,
   secretKey:
     process.env.NODE_ENV === 'development'
       ? process.env.NEXT_PUBLIC_DEV_SECRET_KEY
