95cc01c2a4c354e0069902d30fb5097aad8d1690
Aaron Anama <twikista@gmail.com>
2025-08-14 07:26:24 +0100

    modularized server actions

---
+++ src/lib/actionsV2/articles.js
@@ -0,0 +1,290 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { handleServerSideValidationError } from '../util';
+// import { Article } from '../mongoose/models/article';
+// import { Issue } from '../mongoose/models/issue';
+import { Article, Issue } from '../mongoose/models';
+import { articleSchemaForServer } from '../schemas/issues';
+import { connectDB } from '../mongoose/config';
+import mongoose from 'mongoose';
+import { redirect } from 'next/navigation';
+import { removePdfFromStorage } from '../firebase/services';
+import { cache } from 'react';
+import { auth } from '../../../auth';
+import { deleteFile } from '../r2/servicesActions';
+
+export const getArticle = cache(async (slug, options = null) => {
+  try {
+    await connectDB();
+
+    const article = await Article.findOne(
+      {
+        ref: `${slug.issue}`,
+        slug: `${slug.article}`,
+      },
+      options
+    );
+
+    if (!article) {
+      return null;
+    }
+
+    return article;
+  } catch (error) {
+    console.error('Error fetching article:', error.message);
+    return {
+      ok: false,
+      error: 'Failed to fetch article. Please try again.',
+      errorType: 'other',
+    };
+  }
+});
+
+export const getArticlesInIssue = async (issue, sorted = true) => {
+  await connectDB();
+  if (sorted) {
+    const articlesInIssue = await Article.find({
+      ref: `${issue}`,
+      published: true,
+    }).sort({
+      startPage: 1,
+    });
+    return articlesInIssue;
+  }
+  const articlesInIssue = await Article.find({
+    ref: `${issue}`,
+    published: true,
+  });
+  return articlesInIssue;
+};
+
+export const getAllPublishedArticles = async () => {
+  const publishedArticles = await Article.find({ published: true });
+  return publishedArticles;
+};
+
+export const getArticlesInCurrentIssue = async () => {
+  try {
+    await connectDB();
+
+    const [latestIssue] = await Issue.find({
+      published: true,
+      status: 'published',
+    })
+      .sort({ volume: -1, issueNumber: -1 })
+      .limit(1);
+
+    if (!latestIssue) {
+      return {
+        currentIssue: null,
+        articlesInCurrentIssue: [],
+      };
+    }
+
+    const articlesInCurrentIssue = await Article.find({ ref: latestIssue.ref });
+
+    return {
+      currentIssue: latestIssue,
+      articlesInCurrentIssue,
+    };
+  } catch (error) {
+    console.error('Error fetching current issue articles:', error);
+    return {
+      currentIssue: null,
+      articlesInCurrentIssue: [],
+    };
+  }
+};
+
+export async function createArticle(formData, url, params) {
+  const user = await auth();
+  // Validate form data from frontend
+  const { data, error } = articleSchemaForServer.safeParse(formData);
+  if (error) {
+    const validationError = handleServerSideValidationError(parsedData);
+    return { ok: false, error: validationError, errorType: 'validationError' };
+  }
+
+  const { pdfFile, ...articleData } = data;
+
+  // Add computed fields to article object
+  articleData.pdfUrl = url;
+  articleData.slug = `${articleData.startPage}-${articleData.endPage}`;
+  articleData.ref = `volume-${articleData.volume}-issue-${articleData.issue}`;
+  articleData.published = params.published ? true : false;
+  articleData.publishDate = new Date(params.publishDate);
+  articleData.addedBy = user.user?.firstName + ' ' + user.user?.lastName;
+
+  // Ensure database connection
+  await connectDB();
+
+  // Start a session for the transaction
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction(); // Start the transaction
+
+    // Create new article within transaction
+    const newArticle = new Article(articleData);
+    const savedArticle = await newArticle.save({ session });
+
+    if (!savedArticle?._id) {
+      // Explicitly abort the transaction if article creation fails
+      await session.abortTransaction();
+      return {
+        ok: false,
+        error: 'Failed to create article',
+        errorType: 'other',
+      };
+    }
+
+    // Update issue with new article ID within the same transaction
+    const issueUpdateResult = await Issue.updateOne(
+      {
+        volume: `${savedArticle.volume}`,
+        issueNumber: `${savedArticle.issue}`,
+      },
+      { $push: { articles: savedArticle._id } },
+      { session }
+    );
+
+    if (
+      !issueUpdateResult.acknowledged ||
+      issueUpdateResult.modifiedCount !== 1
+    ) {
+      // Explicitly abort the transaction if issue update fails
+      await session.abortTransaction();
+      return { ok: false, error: 'Failed to update issue', errorType: 'other' };
+    }
+
+    // Commit the transaction if all operations succeed
+    await session.commitTransaction();
+
+    // Revalidate routes affected by article creation to reflect changes
+    revalidatePath(`/archive/${savedArticle.ref}`);
+    revalidatePath(`/dashboard/issues/${savedArticle.ref}`);
+
+    // Send success response back to the client
+    return { ok: true, articleId: JSON.stringify(savedArticle._id) };
+  } catch (error) {
+    console.error('Transaction error:', error);
+    return { ok: false, error: 'Something went wrong', errorType: 'other' };
+  } finally {
+    session.endSession(); // End the session
+  }
+}
+
+export async function deleteArticle(id) {
+  let ref = null;
+  // Ensure database connection
+  await connectDB();
+
+  const session = await mongoose.startSession(); // Start a session for the transaction
+
+  try {
+    session.startTransaction(); // Start the transaction
+
+    // Find and delete the article
+    const deletedArticle = await Article.findByIdAndDelete(id, { session });
+
+    if (!deletedArticle || !deletedArticle._id) {
+      return { ok: false, error: 'Something went wrong', errorType: 'other' };
+    }
+
+    // Update the related issue to remove the article reference
+    const issueUpdateResult = await Issue.updateOne(
+      { ref: deletedArticle.ref },
+      { $pull: { articles: deletedArticle._id } },
+      { session }
+    );
+
+    if (issueUpdateResult.matchedCount === 0) {
+      return { ok: false, error: 'Something went wrong', errorType: 'other' };
+    }
+
+    // Remove the PDF file from storage
+    // await removePdfFromStorage(deletedArticle.pdfUrl);
+    const key = deletedArticle.pdfUrl.split('/').at(-1);
+    const deleted = await deleteFile(key);
+    if (!deleted.success) {
+      return { ok: false, error: 'Something went wrong', errorType: 'other' };
+    }
+
+    // Commit the transaction if all operations succeed
+    await session.commitTransaction();
+
+    // Revalidate affected paths to reflect changes
+    revalidatePath(`/dashboard/issues/${deletedArticle.ref}`);
+    revalidatePath(`/archive/${deletedArticle.ref}`);
+    ref = deletedArticle.ref;
+
+    // Redirect to the updated issues dashboard
+    return { ok: true, ref: deletedArticle.ref };
+  } catch (error) {
+    // Abort the transaction if any operation fails
+    await session.abortTransaction();
+    console.error('Transaction failed:', error.message);
+    return { ok: false, error: 'Something went wrong', errorType: 'other' };
+  } finally {
+    session.endSession(); // End the session
+    redirect(`/dashboard/issues/${ref}`);
+  }
+}
+
+export async function updateArticle(formData, url, id) {
+  //validate form dtata from frontend
+  const { data, error } = articleSchemaForServer.safeParse(formData);
+  if (error) {
+    const validationError = handleServerSideValidationError(parsedData);
+    return { ok: false, error: validationError, errorType: 'validationError' };
+  }
+  //update article fields to reflect changes by user
+
+  const articleData = {
+    ...data,
+    slug: `${data.startPage}-${data.endPage}`,
+    ref: `volume-${data.volume}-issue-${data.issue}`,
+    pdfUrl: url.new !== null ? url.new : url.existing,
+  };
+
+  await connectDB();
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction(); // Start the transaction
+    //check if article exists in database
+    const article = await Article.findById(id);
+    if (!article) {
+      return { ok: false, error: 'Article not found', errorType: 'other' };
+    }
+    //update article in database
+    const updatedArticle = await Article.findByIdAndUpdate(
+      { _id: id },
+      articleData,
+      {
+        new: true,
+      },
+      { session }
+    );
+
+    if (updatedArticle._id === undefined) {
+      await session.abortTransaction();
+      return { ok: false, error: 'Something went wrong', errorType: 'other' };
+    }
+
+    // Commit the transaction if all operations succeed
+    await session.commitTransaction();
+
+    //revalidate all routes to reflect updated data
+    revalidatePath(`/archive/${updatedArticle.ref}`);
+    revalidatePath(`/dashboard/issues/${updatedArticle.ref}`);
+    return { ok: true };
+  } catch (error) {
+    await session.abortTransaction();
+    return { ok: false, error: 'Something went wrong', errorType: 'other' };
+  } finally {
+    session.endSession(); // End the session
+  }
+}
---
+++ src/lib/actionsV2/issues.js
@@ -0,0 +1,337 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { connectDB } from '../mongoose/config';
+import { Article } from '../mongoose/models/article';
+import { Issue } from '../mongoose/models/issue';
+import { auth } from '../../../auth';
+import { IssueFormSchema } from '../schemas/issues';
+import mongoose from 'mongoose';
+import { cache } from 'react';
+
+export const getIssues = async (status = 'published') => {
+  try {
+    await connectDB();
+    const issues = await Issue.find({ status }).sort({
+      volume: -1,
+      issueNumber: -1,
+    });
+    return issues;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getIssue = async (issueRef, options = null) => {
+  try {
+    await connectDB();
+    const issue = await Issue.findOne({ ref: issueRef }, options).lean();
+
+    // const json = JSON.stringify(issue)
+    // const issueObject = JSON.parse(json)
+    return issue;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//create new issue
+export const createIssue = async (formData) => {
+  const user = await auth();
+  const {
+    user: { firstName, lastName },
+  } = user;
+
+  const parsedData = IssueFormSchema.safeParse(formData);
+  if (!parsedData.success) {
+    const validationError = handleServerSideValidationError(parsedData);
+    return { ok: false, error: validationError, errorType: 'validationError' };
+  }
+  const { issueNumber, issueYear, volume, issueType, issueTheme } =
+    parsedData.data;
+  const issueData = {
+    issueType,
+    issueTheme,
+    issueNumber,
+    volume,
+    issueYear,
+    ref: `volume-${volume}-issue-${issueNumber}`,
+    issueTitle: `Vol. ${volume} No. ${issueNumber} (${issueYear})`,
+    published: false,
+    publishDate: new Date(String(issueYear)),
+    addedBy: `${firstName} ${lastName}`,
+  };
+
+  try {
+    await connectDB();
+    const newIssue = new Issue(issueData);
+    const savedIssue = await newIssue.save();
+    if (savedIssue?._id !== null) {
+      revalidatePath('/archive');
+      revalidatePath('/dashboard/issues');
+      return { ok: true };
+    }
+  } catch (error) {
+    console.log(error);
+    return { ok: false, error: 'Something went wrong', errorType: 'other' };
+  }
+};
+
+export async function updateIssue(id, formData) {
+  const { data, error } = IssueFormSchema.safeParse(formData);
+  if (error) {
+    const validationError = handleServerSideValidationError(data);
+    return { ok: false, error: validationError, errorType: 'validationError' };
+  }
+
+  try {
+    await connectDB();
+    const currentIssue = await Issue.findById(JSON.parse(id));
+    if (!currentIssue) {
+      return { ok: false, error: 'Issue not found', errorType: 'other' };
+    }
+
+    const issueData = {
+      // ...currentIssue,
+      ...data,
+      ref: `volume-${data.volume}-issue-${data.issueNumber}`,
+      issueTitle: `Vol. ${data.volume} No. ${data.issueNumber} (${data.issueYear})`,
+    };
+
+    const updatedIssue = await Issue.findByIdAndUpdate(
+      { _id: currentIssue._id },
+      issueData,
+      {
+        new: true,
+      }
+    );
+
+    if (updatedIssue._id === undefined) {
+      console.log(`Issue not found ${currentIssue?.issueNumber}`);
+      return { ok: false, error: 'Something went wrong', errorType: 'other' };
+    }
+    //update all articles associated wtih an issue
+
+    revalidatePath('/dashboard/issues');
+    revalidatePath('/archive');
+    return { ok: true };
+  } catch (error) {
+    return { ok: false, error: 'Something went wrong', errorType: 'other' };
+  }
+}
+
+//send draft editorial board for authorization
+export const submitIssueForPublishing = async (ref) => {
+  try {
+    await connectDB();
+    const issueSubmittedForPublishing = await Issue.findOneAndUpdate(
+      { ref: ref, status: 'draft' },
+      {
+        $set: {
+          status: 'review',
+        },
+      },
+      { new: true }
+    );
+
+    if (issueSubmittedForPublishing?._id) {
+      revalidatePath(`/dashboard/issues/${issueSubmittedForPublishing?.ref}`);
+      return { ok: true };
+    } else {
+      return { ok: false, error: 'something went wrong', errorType: 'other' };
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const rejectRequestToPublishIssue = async (ref) => {
+  // const date = new Date()
+  try {
+    await connectDB();
+    const publishedIssue = await Issue.findOneAndUpdate(
+      { ref: ref, status: 'review' },
+      {
+        $set: {
+          status: 'draft',
+        },
+      },
+      { new: true }
+    );
+
+    if (publishedIssue._id) {
+      revalidatePath(`/dashboard/issues/${publishedIssue.ref}`);
+      return { ok: true };
+    } else {
+      return { ok: false, error: 'something went wrong', errorType: 'other' };
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//publish issue
+export const publishIssue = async (issueRef, publishDate) => {
+  const user = await auth();
+  const session = await mongoose.startSession();
+
+  try {
+    await connectDB();
+    console.log('db connected');
+    // Start transaction
+    session.startTransaction();
+
+    const publishedIssue = await Issue.findOneAndUpdate(
+      { ref: issueRef, status: 'draft' },
+      {
+        $set: {
+          published: true,
+          publishDate: new Date(publishDate),
+          status: 'published',
+          publishedBy: `${user.user?.firstName} ${user.user?.lastName}`,
+        },
+      },
+      { new: true, session }
+    );
+
+    if (!publishedIssue?._id) {
+      // Issue not found or not updated, abort transaction
+      await session.abortTransaction();
+      return {
+        ok: false,
+        error: 'Issue not found or already published',
+        errorType: 'not_found',
+      };
+    }
+
+    const publishedArticles = await Article.updateMany(
+      { ref: issueRef },
+      { $set: { published: true, publishDate: publishedIssue.publishDate } },
+      { session }
+    );
+
+    if (!publishedArticles.acknowledged) {
+      // Problem updating articles, abort transaction
+      await session.abortTransaction();
+      return {
+        ok: false,
+        error: 'Failed to update articles',
+        errorType: 'other',
+      };
+    }
+
+    // If we reach here, both operations were successful, commit the transaction
+    await session.commitTransaction();
+
+    // Revalidate paths after successful transaction
+    revalidatePath(`/dashboard/issues/${publishedIssue.ref}`);
+    revalidatePath(`/dashboard/issues`);
+    revalidatePath(`/archive`);
+    revalidatePath('/sitemap.xml');
+
+    return { ok: true };
+  } catch (error) {
+    // Any error occurred, abort transaction
+    await session.abortTransaction();
+    console.error('Transaction failed:', error);
+    return { ok: false, error: error.message, errorType: 'transaction_failed' };
+  } finally {
+    // End session in all cases
+    await session.endSession();
+  }
+};
+
+export const getPublishedIssues = async () => {
+  try {
+    await connectDB();
+    const publishedIssues = await Issue.find({ published: true }).sort({
+      volume: -1,
+      issueNumber: -1,
+    });
+    return publishedIssues;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export async function deleteIssueWithArticles(id) {
+  // Ensure the database connection
+  await connectDB();
+
+  const session = await mongoose.startSession(); // Start a session for the transaction
+
+  try {
+    session.startTransaction(); // Start the transaction
+
+    // Find and delete the issue
+    const deletedIssue = await Issue.findByIdAndDelete(id, { session });
+
+    if (!deletedIssue) {
+      await session.abortTransaction(); // Abort transaction if issue not found
+      console.error('Issue not found.');
+      return { ok: false, message: 'Issue not found.' };
+    }
+
+    // Delete all associated articles
+    const deleteOutcome = await Article.deleteMany(
+      { ref: deletedIssue.ref },
+      { session }
+    );
+
+    if (
+      deletedIssue.articles.length > 0 && // If articles are associated
+      deleteOutcome.deletedCount === 0 // And no articles were deleted
+    ) {
+      await session.abortTransaction(); // Abort transaction if article deletion fails
+      console.error('Failed to delete associated articles.');
+      return { ok: false, message: 'Failed to delete associated articles.' };
+    }
+
+    // Commit the transaction if all operations succeed
+    await session.commitTransaction();
+
+    // Revalidate paths for updated pages
+    revalidatePath('/archive');
+    revalidatePath('/dashboard/issues');
+
+    console.log('Issue and associated articles successfully deleted.');
+    return { ok: true, message: 'Issue and articles deleted successfully.' };
+  } catch (error) {
+    // Abort transaction in case of any error
+    await session.abortTransaction();
+    console.error('An error occurred during the transaction:', error);
+    return { ok: false, message: 'An error occurred during deletion.' };
+  } finally {
+    session.endSession(); // End the session
+  }
+}
+
+export async function deleteIssueWithNoArticles(id) {
+  try {
+    await connectDB();
+    const deletedIssue = await Issue.findByIdAndDelete(id);
+    if (!!deletedIssue) {
+      revalidatePath('/archive');
+      revalidatePath('/dashboard/issues');
+      return { ok: true };
+    } else {
+      return { ok: false };
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Server action to fetch archive data
+export const getArchive = cache(async () => {
+  try {
+    await connectDB();
+    const archive = await Issue.find({ published: true })
+      .sort({ volume: -1, issueNumber: -1 })
+      .limit(4);
+    return archive;
+  } catch (error) {
+    console.error('Error fetching archive:', error);
+    return [];
+  }
+});
